bb245c59ac185a584ed102420d255e92d3cfb558
Stepoik <83645255+Stepoik@users.noreply.github.com>
2025-06-05 20:10:36 +0300

    ref: updated lock icon

--- uikit/src/commonMain/kotlin/stepan/gorokhov/paylance/uikit/images/Lock.kt
+++ uikit/src/commonMain/kotlin/stepan/gorokhov/paylance/uikit/images/Lock.kt
@@ -10,80 +10,61 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 
-public val Icons.Lock: ImageVector
+val Icons.Lock: ImageVector
 	get() {
-		if (_Lock != null) {
-			return _Lock!!
+		if (_lockOutline != null) {
+			return _lockOutline!!
 		}
-		_Lock = ImageVector.Builder(
-            name = "Lock",
-            defaultWidth = 20.dp,
-            defaultHeight = 21.dp,
-            viewportWidth = 20f,
-            viewportHeight = 21f
-        ).apply {
+		_lockOutline = ImageVector.Builder(
+			name = "LockOutline", defaultWidth = 24.dp, defaultHeight = 24.dp,
+			viewportWidth = 24f, viewportHeight = 24f
+		).apply {
 			path(
-    			fill = null,
-    			fillAlpha = 1.0f,
-    			stroke = SolidColor(Color(0xFF171A1F)),
-    			strokeAlpha = 1.0f,
-    			strokeLineWidth = 1.71429f,
-    			strokeLineCap = StrokeCap.Square,
-    			strokeLineJoin = StrokeJoin.Miter,
-    			strokeLineMiter = 10f,
-    			pathFillType = PathFillType.NonZero
+				fill = SolidColor(Color(0xFF000000)),
+				stroke = null
 			) {
-				moveTo(15f, 9.78572f)
-				lineTo(5f, 9.78572f)
-				curveTo(4.2110f, 9.78570f, 3.57140f, 10.42530f, 3.57140f, 11.21430f)
-				lineTo(3.57143f, 16.9286f)
-				curveTo(3.57140f, 17.71760f, 4.2110f, 18.35710f, 50f, 18.35710f)
-				lineTo(15f, 18.3571f)
-				curveTo(15.7890f, 18.35710f, 16.42860f, 17.71760f, 16.42860f, 16.92860f)
-				verticalLineTo(11.2143f)
-				curveTo(16.42860f, 10.42530f, 15.7890f, 9.78570f, 150f, 9.78570f)
+				// M12,17C10.89,17 10,16.1 10,15C10,13.89 10.89,13 12,13A2,2 0 0,1 14,15A2,2 0 0,1 12,17
+				moveTo(12f, 17f)
+				curveTo(10.89f, 17f, 10f, 16.1f, 10f, 15f)
+				curveTo(10f, 13.89f, 10.89f, 13f, 12f, 13f)
+				arcToRelative(2f, 2f, 0f, false, true, 2f, 2f)
+				arcToRelative(2f, 2f, 0f, false, true, -2f, 2f)
+
+				// M18,20V10H6V20H18
+				moveTo(18f, 20f)
+				lineTo(18f, 10f)
+				lineTo(6f, 10f)
+				lineTo(6f, 20f)
+				lineTo(18f, 20f)
+
+				// M18,8A2,2 0 0,1 20,10V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V10C4,8.89 4.89,8 6,8H7V6A5,5 0 0,1 12,1A5,5 0 0,1 17,6V8H18
+				moveTo(18f, 8f)
+				arcToRelative(2f, 2f, 0f, false, true, 2f, 2f)
+				verticalLineTo(20f)
+				arcToRelative(2f, 2f, 0f, false, true, -2f, 2f)
+				horizontalLineTo(6f)
+				curveTo(4.89f, 22f, 4f, 21.1f, 4f, 20f)
+				verticalLineTo(10f)
+				curveTo(4f, 8.89f, 4.89f, 8f, 6f, 8f)
+				horizontalLineTo(7f)
+				verticalLineTo(6f)
+				arcToRelative(5f, 5f, 0f, false, true, 5f, -5f)
+				arcToRelative(5f, 5f, 0f, false, true, 5f, 5f)
+				verticalLineTo(8f)
+				horizontalLineTo(18f)
+
+				// M12,3A3,3 0 0,0 9,6V8H15V6A3,3 0 0,0 12,3Z
+				moveTo(12f, 3f)
+				arcToRelative(3f, 3f, 0f, false, false, -3f, 3f)
+				verticalLineTo(8f)
+				horizontalLineTo(15f)
+				verticalLineTo(6f)
+				arcToRelative(3f, 3f, 0f, false, false, -3f, -3f)
 				close()
-			}
-			path(
-    			fill = null,
-    			fillAlpha = 1.0f,
-    			stroke = SolidColor(Color(0xFF171A1F)),
-    			strokeAlpha = 1.0f,
-    			strokeLineWidth = 1.71429f,
-    			strokeLineCap = StrokeCap.Square,
-    			strokeLineJoin = StrokeJoin.Miter,
-    			strokeLineMiter = 10f,
-    			pathFillType = PathFillType.NonZero
-			) {
-				moveTo(10f, 15.5f)
-				curveTo(10.7890f, 15.50f, 11.42860f, 14.86040f, 11.42860f, 14.07140f)
-				curveTo(11.42860f, 13.28240f, 10.7890f, 12.64290f, 100f, 12.64290f)
-				curveTo(9.2110f, 12.64290f, 8.57140f, 13.28240f, 8.57140f, 14.07140f)
-				curveTo(8.57140f, 14.86040f, 9.2110f, 15.50f, 100f, 15.50f)
-				close()
-			}
-			path(
-    			fill = null,
-    			fillAlpha = 1.0f,
-    			stroke = SolidColor(Color(0xFF171A1F)),
-    			strokeAlpha = 1.0f,
-    			strokeLineWidth = 1.71429f,
-    			strokeLineCap = StrokeCap.Square,
-    			strokeLineJoin = StrokeJoin.Miter,
-    			strokeLineMiter = 10f,
-    			pathFillType = PathFillType.NonZero
-			) {
-				moveTo(13.5714f, 6.92857f)
-				verticalLineTo(6.21429f)
-				curveTo(13.58080f, 5.27660f, 13.21730f, 4.37360f, 12.56090f, 3.70390f)
-				curveTo(11.90460f, 3.03410f, 11.00910f, 2.65250f, 10.07140f, 2.64290f)
-				horizontalLineTo(10f)
-				curveTo(9.06230f, 2.63350f, 8.15930f, 2.9970f, 7.48960f, 3.65340f)
-				curveTo(6.81980f, 4.30970f, 6.43820f, 5.20520f, 6.42860f, 6.14290f)
-				verticalLineTo(6.92857f)
 			}
 		}.build()
-		return _Lock!!
+		return _lockOutline!!
 	}
 
-private var _Lock: ImageVector? = null
+private var _lockOutline: ImageVector? = null
+
